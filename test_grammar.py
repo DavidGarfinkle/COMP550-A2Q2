1b67c13863468fb59934aabcd79756dfb7a13a5e
DavidGarfinkle <david.etan.garfinkle@gmail.com>
2018-10-14 17:04:21 -0400

    Tests: subject-verb-agreement passing

---
+++ test_grammar.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+import pytest
+from nltk.grammar import CFG
+from nltk.parse.chart import BottomUpChartParser
+
+with open("subject-verb.grammar") as f:
+    grammar = CFG.fromstring(f.read(), encoding="utf-8")
+
+
+@pytest.mark.parametrize("sentence_input", [
+    "Je regarde la television",
+    "Tu regardes la television",
+    "Il regarde la television",
+    "Nous regardons la television",
+    "Vous regardez la television",
+    "Ils regardent la television"])
+def test_subject_verb_agreement(sentence_input):
+
+    parser = BottomUpChartParser(grammar)
+
+    trees = parser.parse(sentence_input.lower().split())
+    assert(next(trees))
+
+
+
+#"Le chat mange le poisson",

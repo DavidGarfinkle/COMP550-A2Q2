1c20b448216dca4a7810bdcfbcfc84db286cd298
DavidGarfinkle <david.etan.garfinkle@gmail.com>
2018-10-15 22:56:26 -0400

    Finished assignment specs

--- test_grammar.py
+++ test_grammar.py
@@ -6,43 +6,55 @@ from nltk.parse.chart import BottomUpChartParser
 with open("subject-verb.grammar") as f:
     grammar = CFG.fromstring(f.read(), encoding="utf-8")
 
+tests = {
+        "subject_verb_agreement": [
+            "Je regarde la television",
+            "Tu regardes la television",
+            "Il regarde la television",
+            "Nous regardons la television",
+            "Vous regardez la television",
+            "Ils regardent la television"],
 
-@pytest.mark.parametrize("sentence_input", [
-    "Je regarde la television",
-    "Tu regardes la television",
-    "Il regarde la television",
-    "Nous regardons la television",
-    "Vous regardez la television",
-    "Ils regardent la television"])
-def test_subject_verb_agreement(sentence_input):
+        "test_noun_phrases_and_proper_names": [
+            "le chat",
+            "la television",
+            "les chats",
+            "les televisions",
+            "Jackie",
+            "Montreal"],
+
+        "test_direct_object_pronouns": [ "il la regarde" ],
+
+        "test_attribute_adjectives": [
+            "le chat noir",
+            "le chat heureux",
+            "le beau chat",
+            "le joli chat",
+            "la derniere semaine",
+            "la semaine derniere",
+            "les chats noirs",
+            "la television noire",
+            "les televisions noires"]}
+
+
+@pytest.mark.parametrize("test", ((test_name, sentence) for test_name, sentences in tests.items() for sentence in sentences))
+def test(test):
+    test_name, sentence = test
 
     parser = BottomUpChartParser(grammar)
 
-    trees = parser.parse(sentence_input.lower().split())
+    trees = parser.parse(sentence.lower().split())
     assert(next(trees))
 
 
 @pytest.mark.parametrize("sentence_input", [
-    "le chat",
-    "la television",
-    "les chats",
-    "les televisions",
-    "Jackie",
-    "Montreal"])
-def test_noun_phrases_and_proper_names(sentence_input):
+    "je mangent le poisson",
+    "les noirs chats mangent le poisson",
+    "la poisson mangent les chats",
+    "je mange les"])
+def test_rejection_set(sentence_input):
 
     parser = BottomUpChartParser(grammar)
 
     trees = parser.parse(sentence_input.lower().split())
-    assert(next(trees))
-
-@pytest.mark.parametrize("sentence_input", [
-    "il la regarde"])
-def test_direct_object_pronouns(sentence_input):
-
-    parser = BottomUpChartParser(grammar)
-
-    trees = parser.parse(sentence_input.lower().split())
-    assert(next(trees))
-
-#"Le chat mange le poisson",
+    pytest.raises(StopIteration)
